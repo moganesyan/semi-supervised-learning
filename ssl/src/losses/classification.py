a9067c7bd05b46b8ad04a08460acd742dedc7e89
moganesyan <mishaogan1@hotmail.com>
2022-11-06 01:26:43 +0000

    added masked CCE loss

--- ssl/src/losses/classification.py
+++ ssl/src/losses/classification.py
@@ -19,3 +19,32 @@ def categorical_cross_entropy(y_pred: tf.Tensor, y_test: tf.Tensor) -> tf.Tensor
         axis = -1
     )
     return tf.reduce_mean(-cce)
+
+
+def categorical_cross_entropy_masked(y_pred: tf.Tensor,
+                                     y_test: tf.Tensor,
+                                     mask: tf.Tensor) -> tf.Tensor:
+    """
+        Masked categorical cross-entropy loss.
+
+        args:
+            y_pred (tf.Tensor): Output prediction vector.
+            y_test (tf.Tensor): Ground truth vector.
+            mask (tf.Tensor): Mask vector for "switching off" loss
+                for certain samples.
+        returns:
+            loss (tf.Tensor): Categorical CE loss.
+    """
+
+    mask_num = tf.cast(mask, tf.float32)
+    _epsilon = tf.constant(1e-16)
+
+    cce = -1 * tf.reduce_sum(
+        y_test * tf.math.log(y_pred + _epsilon),
+        axis = -1
+    )
+    cce_maksed = cce * mask_num
+
+    # calculate modified batchwise mean
+    minibatch_size = tf.reduce_sum(mask_num)
+    return tf.reduce_sum(cce) / (minibatch_size + _epsilon)
